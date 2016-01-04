8188481588ff21b4e914bf242f5053e4f4022ef3
Michael Todd <mikejtodd@gmail.com>
2016-01-03 22:30:31 -0600

    Adds scroll navigation for posters

--- app/assets/javascripts/gigs.js
+++ app/assets/javascripts/gigs.js
@@ -1,6 +1,5 @@
 $(function() {
     $(".gigs--posters").swipe( {
-        //Generic swipe handler for all directions
         swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
             if (direction == "up") {
                 var topPoster = $('.top');
@@ -19,4 +18,34 @@ $(function() {
             }
         }
     });
+    
+    var mousewheelStopped = true;
+    
+    $(document).bind('mousewheel', function(evt) {
+        var delta = evt.originalEvent.wheelDelta;
+        
+        if (delta == 0) {
+            mousewheelStopped = true;
+        }
+        
+        setTimeout(function(){
+            if (delta > 10 && mousewheelStopped == true) {
+                var topPoster = $('.top')
+                if (topPoster.next().first().hasClass('posters--event')) {
+                    topPoster.next().first().removeClass('hide');
+                    topPoster.next().first().addClass('top');
+                    topPoster.removeClass('top');
+                }
+                mousewheelStopped = false;
+            } else if (delta < -10 && mousewheelStopped == true) {
+                var topPoster = $('.top');
+                if (topPoster.prev().first().hasClass('posters--event')) {
+                    topPoster.addClass('hide');
+                    topPoster.prev().first().addClass('top');
+                    topPoster.removeClass('top');
+                }
+                mousewheelStopped = false;
+            }
+        }, 100);
+    })
 });
